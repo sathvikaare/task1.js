d5e3bd622c4ba9761b89bebdea9c9388167a35bc
Are Sathvika <sathvikaare27@gmail.com>
2024-05-02 18:30:32 +0530

    file

---
+++ task1.js
@@ -0,0 +1,32 @@
+let a=30;
+let b= 20;
+console.log(`sum of ${a} and ${b} is ${a+b}`);
+console.log(`sub of ${a} and ${b} is ${a-b}`);
+console.log(`multiplication of ${a} and ${b} is ${a*b}`);
+console.log(`division of ${a} and ${b} is ${a/b}`);
+console.log(`remainder of ${a} and ${b} is ${a%b}`);
+let i=5;
+j=++i;
+console.log(`increment of i  is ${j}`);
+let k=7;
+n=--k;
+console.log(`increment of k  is ${n}`);
+let a1=7;
+let a2=8;
+a1%2==0?console.log("number is even"):console.log("number is odd");
+a1==a2?console.log("true"):console.log("false");
+a1===a2?console.log("true"):console.log("false");
+
+a1>a2?console.log("a1 is greater"):console.log("a2 is greater");
+a1<a2?console.log("a1 is smaller"):console.log("a2 is smaller");
+let a3=5;
+a3>=0?console.log("Number is +ve"):console.log("number is -ve");
+a1<=a2?console.log("a1 is smaller"):console.log("a2 is smaller");
+a1!=a2?console.log("a1 is not equal"):console.log("a2 and a2 are equal");
+a1>=a2?console.log("a1 is greater"):console.log("a2 is greater");
+a1%a2==0?console.log(`${a1} is divisible by ${a2}`):console.log(`${a1} is not divisible by ${a2}`);
+
+
+
+
+
